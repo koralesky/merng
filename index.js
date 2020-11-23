bb11ab3716185cb593833a1eb44eebd75544a8b3
Dominik <dominik.koralewski@gmail.com>
2020-11-23 19:34:40 +0100

    creating models, setting up server

---
+++ index.js
@@ -0,0 +1,53 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const Post = require('./models/Post');
+
+
+const dbConnection = process.env.DB_CONNECTION;
+
+const typeDefs = gql`
+    type Post{
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
+    type Query{
+        getPosts: [Post]
+    }
+`;
+
+const resolvers = {
+    Query: {
+        async getPosts(){
+            try{
+                const posts = await Post.find();
+                return posts;
+            } catch(err){
+                throw new Error(err);
+            }
+        }
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+
+mongoose
+    .connect(dbConnection, {useNewUrlParser: true})
+    .then(() => {
+        console.log('MongoDB connected')
+        return server.listen({port:5000});
+    })
+    .then(res => {
+        console.log(`Server runing at ${res.url}`)
+    });
+    
+
+    
