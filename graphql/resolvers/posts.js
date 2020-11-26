d41d4f140a05d2c22af84e897b2edba35b3cb1af
Dominik <dominik.koralewski@gmail.com>
2020-11-26 21:02:48 +0100

    react installed

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,5 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
+const { argsToArgsConfig } = require("graphql/type/definition");
 
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
@@ -27,9 +28,13 @@ module.exports = {
     },
   },
   Mutation: {
+    //create post
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
-      console.log(user);
+
+      if (args.body.trim === "") {
+        throw new Error("Post body must not be empty");
+      }
 
       const newPost = new Post({
         body,
@@ -39,23 +44,54 @@ module.exports = {
       });
 
       const post = await newPost.save();
+
+      context.pubsub.publish("NEW_POST", {
+        newPost: post,
+      });
+
       return post;
     },
-  },
+    //delete post
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
 
-  async deletePost(_, { postId }, context) {
-    const user = checkAuth(context);
-
-    try {
-      const post = await Post.findById(postId);
-      if (user.username == post.username) {
-        await post.delete();
-        return "post deleted succesfully";
-      } else {
-        throw new AuthenticationError("Action not allowed");
+      try {
+        const post = await Post.findById(postId);
+        if (user.username == post.username) {
+          await post.delete();
+          return "post deleted succesfully";
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } catch (err) {
+        throw new Error(err);
       }
-    } catch (err) {
-      throw new Error(err);
-    }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          //Post already liked, unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          //Not liked, like post
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError("Post not found");
+      }
+    },
+  },
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator("NEW_POST"),
+    },
   },
 };
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer, PubSub } = require("apollo-server");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers/index");
 const dbConnection = process.env.DB_CONNECTION;
+
+const pubsub = new PubSub();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({ req }),
+  context: ({ req }) => ({ req, pubsub }),
 });
 
 mongoose
